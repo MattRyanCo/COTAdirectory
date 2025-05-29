d51432c41267f5ac8a6363661e92da408a1cceb5
Matt Ryan <mattryanco@proton.me>
2025-05-29 17:53:13 -0400

    Add update family functionality

--- app-includes/edit_family.php
+++ app-includes/edit_family.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["familyname"])) {
     $stmt->close();
 
     if (!$family) {
-        die("Family not found.");
+        die("Family not found. Try again or Return to the <a href='index.php'>main menu</a>.");
     }
 
     // Fetch members
@@ -39,37 +39,40 @@ if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["familyname"])) {
     <form action="update_family.php" method="post">
         <input type="hidden" name="family_id" value="<?= $family['id'] ?>">
         <label>Family Name:</label>
-        <input type="text" name="familyname" value="<?= $family['familyname'] ?>" required><br>
+        <input type="text" name="familyname" value="<?= htmlspecialchars($family['familyname']) ?>" required><br>
         <label>Address:</label>
-        <input type="text" name="address" value="<?= $family['address'] ?>"><br>
+        <input type="text" name="address" value="<?= htmlspecialchars($family['address']) ?>"><br>
         <label>City:</label>
-        <input type="text" name="city" value="<?= $family['city'] ?>"><br>
+        <input type="text" name="city" value="<?= htmlspecialchars($family['city']) ?>"><br>
         <label>State:</label>
-        <input type="text" name="state" value="<?= $family['state'] ?>"><br>
+        <input type="text" name="state" value="<?= htmlspecialchars($family['state']) ?>"><br>
         <label>Zip Code:</label>
-        <input type="text" name="zip" value="<?= $family['zip'] ?>"><br>
+        <input type="text" name="zip" value="<?= htmlspecialchars($family['zip']) ?>"><br>
         <label>Home Phone:</label>
-        <input type="text" name="homephone" value="<?= $family['homephone'] ?>"><br>
+        <input type="text" name="homephone" value="<?= htmlspecialchars($family['homephone']) ?>"><br>
         <label>annday (MM/DD):</label>
-        <input type="text" name="annday" value="<?= $family['annday'] ?>"><br>
+        <input type="text" name="annday" value="<?= htmlspecialchars($family['annday']) ?>"><br>
 
         <h3>Members</h3>
         <?php while ($member = $members->fetch_assoc()): ?>
             <div>
-                <label>Name:</label>
-                <input type="text" name="members[first_name][]" value="<?= $member['first_name'] ?>">
-                <label>Cell Phone:</label>
-                <input type="text" name="members[cell_phone][]" value="<?= $member['cell_phone'] ?>">
-                <label>email:</label>
-                <input type="email" name="members[email][]" value="<?= $member['email'] ?>">
+                <input type="hidden" name="members[id][]" value="<?= $member['id'] ?>">
+                <label>Name: </label>
+                <input type="text" name="members[first_name][]" value="<?= htmlspecialchars($member['first_name']) ?>">
+                <input type="text" name="members[last_name][]" value="<?= !empty($member['last_name']) ? htmlspecialchars($member['last_name']) : htmlspecialchars($family['familyname'] ?? '') ?>">
+                <label>Cell: </label>
+                <input type="text" name="members[cell_phone][]" value="<?= htmlspecialchars($member['cell_phone']) ?>">
+                <label>Email:</label>
+                <input type="email" name="members[email][]" value="<?= htmlspecialchars($member['email']) ?>">
                 <label>Birthday:</label>
-                <input type="text" name="members[birthday][]" value="<?= $member['birthday'] ?>">
+                <input type="text" name="members[birthday][]" value="<?= htmlspecialchars($member['birthday']) ?>">
                 <label>Baptism:</label>
-                <input type="text" name="members[baptism][]" value="<?= $member['baptism'] ?>"><br>
+                <input type="text" name="members[baptism][]" value="<?= htmlspecialchars($member['baptism']) ?>"><br>
             </div>
         <?php endwhile; ?>
 
         <button type="submit">Update</button>
     </form>
+    <br><p><a href='index.php'>Return to main menu</a></p>
 </body>
 </html>
