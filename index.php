3d557538dbf89738fc7dd776b51fdc345e3ee700
Matt Ryan <mattryanco@proton.me>
2025-07-18 13:02:51 -0400

    Update version and changelog

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * App Name:    Family Directory Management
  * Description: A simple family directory management system.
- * Version:     2.0.4.1 
+ * Version:     2.0.5 
  * 
  * @package     FamilyDirectory
  * @author      Matt Ryan
