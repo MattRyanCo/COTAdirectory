5f328226f9d5124781ad8d39ff4365e7bd53ad1f
Matt Ryan <mattryanco@proton.me>
2025-08-09 13:07:13 -0400

    Correct typo in variable use. Remove debug var_dumps.

--- app-includes/bootstrap.php
+++ app-includes/bootstrap.php
@@ -1,17 +1,9 @@
 <?php
-var_dump( __DIR__ . '/class-cota-constants.php' );
 require_once __DIR__ . '/class-cota-constants.php';
 $constants = new Constants();
-var_dump( $constants::COTA_APP_INCLUDES );
-var_dump( $constants::COTA_APP_ASSETS );
-var_dump( $constants::COTA_APP_LIBRARIES );
-var_dump( $constants::MAX_INFORMATIONAL_DOCS );
-var_dump( $constants::UPLOAD_DIR );
-var_dump( $constants->COTA_APP_FILE );
 
-var_dump( __DIR__ . '/class-app-meta-data.php' );
 require_once __DIR__ . '/class-app-meta-data.php';
-$meta = new App_Meta_Data( $constants->COTA_APP_FILE );
+$meta = new App_Meta_Data( $constants->$COTA_APP_FILE );
 
 // Class constants (defined with const) are accessed as
 // ClassName::CONSTANT_NAME.
