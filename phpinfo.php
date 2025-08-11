314681c2ef3b5d0dfeeab887dd1961af16f5082e
Matt Ryan <mattryanco@proton.me>
2025-08-11 12:48:52 -0400

    phpinfo testing

---
+++ phpinfo.php
@@ -0,0 +1,11 @@
+<?php
+// List error constants defined. 
+// $pot = 0;
+// foreach (array_reverse(str_split(decbin(error_reporting()))) as $bit) {
+//     if ($bit == 1) {
+//         echo array_search(pow(2, $pot), get_defined_constants(true)['Core']). "<br>\n";
+//     }
+//     $pot++;
+// }
+// xdebug_info();
+phpinfo();
