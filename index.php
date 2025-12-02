dbc8c62bfe49c9e2322c161cc45ca287e0af1cdc
Matt Ryan <matt@capwebsolutions.com>
2025-12-02 14:04:54 -0500

    Adjust styling of password reset pages.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * App Name:    Family Directory Management
  * Description: A simple family directory management system.
- * Version:     4.0.1
+ * Version:     4.0.2
  * 
  * @package     FamilyDirectory
  * @author      Matt Ryan
