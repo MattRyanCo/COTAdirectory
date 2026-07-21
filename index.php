6db510cab071e03959bba2aa049f35c9015fce04
Matt Ryan <matt@mattryan.co>
2026-07-21 10:31:29 -0400

    Fixed to correct desktop styling issues introduced in 4.1.9

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * App Name:    Family Directory Management
  * Description: A simple family directory management system.
- * Version:     4.1.10
+ * Version:     4.1.11
  * 
  * @package     FamilyDirectory
  * @author      Matt Ryan
